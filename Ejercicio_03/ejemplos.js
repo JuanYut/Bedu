4a582b6a38118cf41326e1af715672db1874b5be
JuanYut <juanyut97@hotmail.com>
2019-08-24 09:12:43 -0500

    ejercicio 04 y 05

--- Ejercicio_03/ejemplos.js
+++ Ejercicio_03/ejemplos.js
@@ -1,5 +1,6 @@
 let cars = ['BMW', 'Vocho', 'Burro', 'Tesla']
 
+/*
 
 for (let i = 5; i > 0; i--) {
   console.log(i)
@@ -8,3 +9,78 @@ for (let i = 5; i > 0; i--) {
 for(car of cars) {
   console.log(car)
 }
+
+let letter = ''
+for (let i = 0; i < 20; i++) {
+  console.log(letter += '*')
+}
+
+for (let i = 20; i > 0; i--) {
+  let letter_02 = ''
+  for (let j = 0; j < i; j++) {
+    letter_02 += '*'
+  }
+  console.log(letter_02)
+}
+
+*/
+
+const generateRandom = num => Math.ceil(Math.random() * num)
+
+const knowMulti = num => {
+  if ( ((num % 3) === 0) && ((num % 5) !== 0) ) {
+    return `${num} - FIZZ`
+  }
+  else if ( (((num % 3) !== 0) && ((num % 5) === 0) )) {
+    return `${num} - BUZZ`
+  }
+  else if ( ((num % 3) === 0) && ((num % 5) === 0) ) {
+    return `${num} - FIZZ and BUZZ`
+  } else {
+    return `${num} - Nothing`
+  }
+
+}
+console.log( knowMulti( generateRandom(15) ) )
+
+const generateFibonacci = num => {
+  let antes = 0, despues = 1, temp
+  let resu = '0, '
+  while( num > 0) {
+    temp = despues
+    despues += antes
+    antes = temp
+    resu +=`${antes}, `
+    num--
+  }
+  return resu
+}
+console.log(generateFibonacci(8))
+
+const generateHand = () => {
+  switch ( generateRandom(3) ) {
+    case 1:
+      return 'Papel'
+    case 2:
+      return 'Roca'
+    case 3:
+      return 'Tijera'
+  }
+}
+
+const knowTheWinner = (p1, p2) => {
+  let resu = ''
+  if (p1 === p2) {
+    resu = `Empate con ${p1} y ${p2}`
+  } else {
+    if (p1 === 'Papel' && p2 === 'Roca' ||
+        p1 === 'Roca' && p2 === 'Tijera' ||
+        p1 === 'Tijera' && p2 === 'Papel' ) {
+      resu = `Player 1 con ${p1} vence a Player 2 con ${p2}`
+    } else {
+      resu = `Player 2 con ${p2} vence a Player 1 con ${p1}`
+    }
+  }
+  return resu
+}
+console.log( knowTheWinner( generateHand(), generateHand() ) )
---
+++ index.js
@@ -0,0 +1,3 @@
+// ES6
+let cars = ['BMW', 'Vocho', 'Burro', 'Tesla']
+console.log( cars.map(leng => leng.length) )
